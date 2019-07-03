96b5b742ec146f28698dd5c87a94533387a81245
Denis Zhokhov <d3z41k@gmail.com>
2019-07-03 18:18:37 +0200

    Add wait interrupt and graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -26,5 +29,16 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 
+	// Start the server
 	go srv.ListenAndServe()
+
+	// Wait for an interrupt
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	// Attempt a graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	srv.Shutdown(ctx)
 }
