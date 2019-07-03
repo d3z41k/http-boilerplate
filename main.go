3c51594b10c7721a940d32838157d8a2654bd401
Denis Zhokhov <d3z41k@gmail.com>
2019-07-03 18:14:56 +0200

    Add chi-router

--- main.go
+++ main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+func NewRouter() http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world!")
 	})
+	return r
+}
 
-	http.ListenAndServe(":8080", nil)
+func main() {
+	handler := NewRouter()
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      handler,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go srv.ListenAndServe()
 }
